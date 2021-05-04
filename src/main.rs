61fc27363fb18fa73b4da72bba748be31b035330
David Strauß <david.strauss@edgycircle.com>
2021-05-04 16:58:24 +0200

    Refactor to extracted `Tui` struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::io::{self, stdout};
-use termion::raw::IntoRawMode;
-use termion::input::TermRead;
-use termion::event::Key;
+mod tui;
+use tui::Tui;
 /*
 Tui
 Store
@@ -14,28 +12,6 @@ Task
 Next step is to move everything in `main` to a file called `tui.rs`.
 */
 fn main() {
-    let actions = vec!["Learn Rust", "Learn Go", "Convince Thomas"];
-    let number_of_actions = actions.len();
-
-    match number_of_actions {
-        0 => println!("Nothing left to do, great job!"),
-        1 => println!("There is one action:"),
-        _ => println!("There are {} actions:", number_of_actions),
-    }
-
-    for action in actions.iter() {
-        println!("{}", action);
-    }
-
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Esc => break,
-                key => println!("{:?}", key),
-            },
-            Err(error) => panic!(error),
-        }
-    }
+    let tui = Tui {};
+    tui.run();
 }
---
+++ src/tui.rs
@@ -0,0 +1,35 @@
+use std::io::{self, stdout};
+use termion::raw::IntoRawMode;
+use termion::input::TermRead;
+use termion::event::Key;
+
+pub struct Tui {}
+
+impl Tui {
+    pub fn run(&self) {
+        let actions = vec!["Learn Rust", "Learn Go", "Convince Thomas"];
+        let number_of_actions = actions.len();
+
+        match number_of_actions {
+            0 => println!("Nothing left to do, great job!"),
+            1 => println!("There is one action:"),
+            _ => println!("There are {} actions:", number_of_actions),
+        }
+
+        for action in actions.iter() {
+            println!("{}", action);
+        }
+
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Esc => break,
+                    key => println!("{:?}", key),
+                },
+                Err(error) => panic!(error),
+            }
+        }
+    }
+}
