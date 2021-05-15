880652830e3391693c5668dfd287554bc79c861e
David Strauß <david.strauss@edgycircle.com>
2021-05-15 13:52:25 +0200

    Remove obsolete comments

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,7 @@ use tui::Tui;
 mod terminal;
 use terminal::Terminal;
 mod task;
-/*
-Tui
-Store
-Keyboard
-Terminal
-TaskListScreen
-TaskFormScreen
-Task
 
-Next step is to move everything in `main` to a file called `tui.rs`.
-*/
 fn main() {
     let terminal = Terminal::default();
     let mut tui = Tui::default(&terminal);
